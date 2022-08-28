0116273001244d63d7bc8dcfd30d37d90e0e3b28
Brett Terpstra <me@brettterpstra.com>
2022-08-28 15:45:04 -0500

    formula update

--- Formula/gather-cli.rb
+++ Formula/gather-cli.rb
@@ -1,5 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class GatherCli < Formula
   desc "Readability and Markdown utility for saving web urls and HTML text"
   homepage "https://brettterpstra.com/projects/gather-cli/"
@@ -7,11 +5,10 @@ class GatherCli < Formula
   sha256 "97e2e879d36c8b989b535517ed58de6d5f9166c65ab582ee89d823f5c02f8157"
   license "MIT"
 
-
   def install
-    FileUtils.mkdir("bin")
-    FileUtils.cp("gather", "bin")
-    bin.install("gather")
+    # mkdir("bin")
+    # cp("gather", "bin")
+    install("gather")
   end
 
   test do
