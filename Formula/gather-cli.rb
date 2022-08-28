ef814e88d622655a8c0539c0cd6bb9611db1c3be
Brett Terpstra <me@brettterpstra.com>
2022-08-28 15:48:35 -0500

    formula test update

--- Formula/gather-cli.rb
+++ Formula/gather-cli.rb
@@ -5,14 +5,17 @@ class GatherCli < Formula
   sha256 "97e2e879d36c8b989b535517ed58de6d5f9166c65ab582ee89d823f5c02f8157"
   license "MIT"
 
+  bottle do
+    root_url "https://ttscoff.jfrog.io/artifactory/bottles-thelab"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "53ee2ccba6b212402bb49f4fc0232e82c940b0e9cca88298d8547539f1c58597"
+  end
+
   def install
-    # mkdir("bin")
-    # cp("gather", "bin")
-    install("gather")
+    bin.install("gather")
   end
 
   test do
     system "gather", "https://brettterpstra.com", "--file", "test.txt"
-    assert_predicate testpath/"test.txt", :exist?
+    assert_predicate "test.txt", :exist?
   end
 end
