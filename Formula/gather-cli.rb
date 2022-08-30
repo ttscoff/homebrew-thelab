89cfca5873b02512ae808ec5d92214e1019778f3
Brett Terpstra <me@brettterpstra.com>
2022-08-30 15:25:08 -0500

    updated formula

--- Formula/gather-cli.rb
+++ Formula/gather-cli.rb
@@ -1,23 +1,18 @@
 class GatherCli < Formula
   desc "Readability and Markdown utility for saving web urls and HTML text"
   homepage "https://brettterpstra.com/projects/gather-cli/"
-  url "https://cdn3.brettterpstra.com/downloads/gather-cli2.0.8.zip"
-  sha256 "97e2e879d36c8b989b535517ed58de6d5f9166c65ab582ee89d823f5c02f8157"
+  url "https://github.com/ttscoff/gather-cli/archive/refs/tags/2.0.24.tar.gz"
+  sha256 "60b07f37ecd1c99ef658a7f4a9b6f0831a822ffee65e3d6164b824c9c2c45e17"
   license "MIT"
 
-  bottle do
-    root_url "https://ttscoff.jfrog.io/artifactory/bottles-thelab"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "3c7722c14e7a14d0e6e1b97985968e6bb71b01b3edf1021b56715daa1bdfa384"
-  end
+  depends_on "swift" => :build
 
   def install
-    # mkdir("bin")
-    # mv("gather", "bin")
-    bin.install("gather")
+    system "xcrun", "swift", "build", "-c", "release"
+    bin.install ".build/apple/Products/Release/gather"
   end
 
   test do
-    # assert_equal "Saved to file: test.txt", shell_output("#{bin}/gather https://brettterpstra.com --file test.txt").strip
-    system "true"
+    system "false"
   end
 end
