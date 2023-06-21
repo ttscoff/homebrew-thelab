a18ba707f100fa409b137f5031a8579c902657e4
Brett Terpstra <me@brettterpstra.com>
2023-06-21 15:45:55 -0500

    Formula update 2.1.5

--- Formula/gather-cli.rb
+++ Formula/gather-cli.rb
@@ -2,7 +2,7 @@ class GatherCli < Formula
   desc "Readability and Markdown utility for saving web urls and HTML text"
   homepage "https://brettterpstra.com/projects/gather-cli/"
   url "https://github.com/ttscoff/gather-cli.git",
-    tag: "2.1.4", revision: "ede1a94c0157e3f0e5c36ecbfd2a2307c4b22ca4"
+    tag: "2.1.5", revision: "30c00b26d547f76d1d09be7ab13464680878a9f7"
   head "https://github.com/ttscoff/gather-cli.git"
 
   depends_on xcode: ["10.0", :build]
