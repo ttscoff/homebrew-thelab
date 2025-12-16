8a93e89964df210cb7bf3346c8299b2fbdb087d1
Brett Terpstra <me@brettterpstra.com>
2025-12-16 12:09:45 -0600

    Update gather-cli to 2.1.10

--- Formula/gather-cli.rb
+++ Formula/gather-cli.rb
@@ -7,12 +7,12 @@
 class GatherCli < Formula
   desc "Readability and Markdown utility for saving web urls and HTML text"
   homepage "https://brettterpstra.com/projects/gather-cli/"
-  version "2.1.9"
+  version "2.1.10"
   license "MIT"
 
   on_macos do
     url "https://github.com/ttscoff/gather-cli/releases/download/v#{version}/gather-cli-#{version}-macos-universal.tar.gz"
-    sha256 "5a3d5922ebe5b5a936cd1276534c20811d184be68836526d54a9c036651df2e5"
+    sha256 "55fe5e2243bfb0166a04f8e3234c02d012e510fe7776e7fc770231cee2884119"
   end
 
   def install
