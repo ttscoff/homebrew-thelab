2c910cd57c9ad9adaef616ddede968636d23fa42
Brett Terpstra <me@brettterpstra.com>
2026-01-04 11:17:18 -0600

    Update apex to 0.1.50

--- Formula/apex.rb
+++ Formula/apex.rb
@@ -7,14 +7,14 @@
 class Apex < Formula
   desc "Unified Markdown processor supporting CommonMark, GFM, MultiMarkdown, and Kramdown"
   homepage "https://github.com/ApexMarkdown/apex"
-  version "0.1.49"
+  version "0.1.50"
   license "MIT"
 
   depends_on "libyaml"
 
   on_macos do
     url "https://github.com/ApexMarkdown/apex/releases/download/v#{version}/apex-#{version}-macos-universal.tar.gz"
-    sha256 "7f6e5039227eb62ae24f1a01689ef7ccf0ddd78b334324b54a97a0c182d4af52"
+    sha256 "1f80cf44f04647b6437a8f7e527c0aacdb10cb8fe4e30318062d3c24edb36c38"
   end
 
   def install
