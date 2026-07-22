42a35347fb755f2a84749ff1a4a5f96c88292b43
Brett Terpstra <me@brettterpstra.com>
2026-07-22 05:25:47 -0500

    na v1.0.4

--- Formula/na.rb
+++ Formula/na.rb
@@ -2,20 +2,20 @@ class Na < Formula
   desc "TaskPaper next-action CLI"
   homepage "https://github.com/ttscoff/na_rust"
   license "MIT"
-  version "1.0.2"
+  version "1.0.4"
 
   on_macos do
-    url "https://github.com/ttscoff/na_rust/releases/download/v1.0.2/na-macos-universal.tar.gz"
-    sha256 "9928bfcec6a5727f2419e4f443cf75a0f3c5ae5d1e6a9aa34887cd14fcdff95b"
+    url "https://github.com/ttscoff/na_rust/releases/download/v1.0.4/na-macos-universal.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ttscoff/na_rust/releases/download/v1.0.2/na-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "54213663482a50edae5a2034d515a434f8c3ec03980bae6048a4b5eb4f2b33a6"
+      url "https://github.com/ttscoff/na_rust/releases/download/v1.0.4/na-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/ttscoff/na_rust/releases/download/v1.0.2/na-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8baa859e4ddb8b85a927fcdd0068e1d7f1c4d8838e9d302f2d8b25ce8913ddc5"
+      url "https://github.com/ttscoff/na_rust/releases/download/v1.0.4/na-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
