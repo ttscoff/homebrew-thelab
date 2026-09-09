be92c76517eb18194f52437d6ee41439be106875
Brett Terpstra <me@brettterpstra.com>
2026-09-09 09:20:42 -0500

    Update screencoord to 0.2.0

--- Formula/screencoord.rb
+++ Formula/screencoord.rb
@@ -5,8 +5,8 @@
 class Screencoord < Formula
   desc "Select a screen region and print coordinates for screencapture or AppleScript"
   homepage "https://github.com/ttscoff/screencoord"
-  url "https://github.com/ttscoff/screencoord/releases/download/v0.1.1/screencoord-0.1.1-macos-universal.tar.gz"
-  sha256 "004bffc4ef644af2c24133829c431c242168e01b0c42a2af2692ec618d05dee2"
+  url "https://github.com/ttscoff/screencoord/releases/download/v0.2.0/screencoord-0.2.0-macos-universal.tar.gz"
+  sha256 "04fc411743ae9e4911b51fdd3b16fc880a72141af00398f2400b666ee2353067"
   license "MIT"
 
   depends_on :macos
